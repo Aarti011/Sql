07b9142b525aade4de9f90dbf064993e1b49e431
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-06-01 08:24:14 +0530

    Update third.sql

--- third.sql
+++ third.sql
@@ -1,1 +1,25 @@
 -- working on tables 
+-- full join 
+SELECT column_name(s)
+FROM table1
+FULL JOIN table2
+ON table1.column_name = table2.column_name
+WHERE condition;
+
+
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID;
+
+--self join
+SELECT column_name(s)
+FROM table1 T1, table1 T2
+WHERE condition;
+
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
+
